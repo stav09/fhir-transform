550a6448050952e6ea0cf023912199b5c2d154a6
Steve Crow <stav.09@gmail.com>
2015-03-26 15:31:15 +1000

    Use command line args for input and output files

--- FHIR Transform/src/cti/fhir/Main.java
+++ FHIR Transform/src/cti/fhir/Main.java
@@ -8,18 +8,15 @@ import cti.fhir.model.DataElement;
 
 public class Main {
 
-	public static final String INPUT_FILE_PATH = "~/Downloads/Haematology.csv";
-	public static final String OUTPUT_FILE_PATH = "Haematology.json";
-	
 	public static void main(String ... args) throws Exception {
 		
 		List<DataElement> elements = 
-				CsvFile.process(INPUT_FILE_PATH, Transform::toDataElement);
+				CsvFile.process(args[0], Transform::toDataElement);
 		
 		Bundle bundle = new Bundle();
 		bundle.addEntries(elements);
 		
-		JsonFile.write(OUTPUT_FILE_PATH, bundle);
+		JsonFile.write(args[1], bundle);
 	}
 
 }
