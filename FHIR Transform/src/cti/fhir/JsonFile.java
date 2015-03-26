3e8d11a33ed2a9037e6f14c1de8339c41c7777c6
Steve Crow <stav.09@gmail.com>
2015-03-26 15:30:51 +1000

    Use pretty printing

--- FHIR Transform/src/cti/fhir/JsonFile.java
+++ FHIR Transform/src/cti/fhir/JsonFile.java
@@ -4,12 +4,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class JsonFile {
 
 	public static void write(String filePath, Object obj) throws IOException {
 		
-		String json = new Gson().toJson(obj);
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(obj);
 			
 		try (FileWriter writer = new FileWriter(filePath)) {
 			 writer.write(json);
